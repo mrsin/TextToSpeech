b7b79cec09e8bd749dbc9a8c0b250073d411bbb3
Sergey Sinitsa <info@drupal-coder.ru>
2015-11-19 07:14:32 +0300

    Initial commit

---
+++ app/src/main/java/com/klab/sin/texttospeech/MainActivity.java
@@ -0,0 +1,44 @@
+package com.klab.sin.texttospeech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private Button speakNowButton;
+    private EditText editText;
+    TTSManager ttsManager = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ttsManager = new TTSManager();
+        ttsManager.init(this);
+
+        editText = (EditText) findViewById(R.id.input_text);
+        speakNowButton = (Button) findViewById(R.id.speak_now);
+        speakNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                String text = editText.getText().toString();
+                ttsManager.initQueue(text);
+            }
+        });
+
+    }
+
+    /**
+     * Releases the resources used by the TextToSpeech engine.
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        ttsManager.shutDown();
+    }
+}
